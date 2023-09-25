adcad79a9f4381b29c0e2e2838b1ad8ab7952365
Hariharan-raj <shankarraj.h@northeastern.edu>
2023-09-25 15:03:55 -0400

    added the response header no cache

--- health_check_api/src/controllers/healthController.js
+++ health_check_api/src/controllers/healthController.js
@@ -3,7 +3,10 @@ const healthService = require("../services/healthService");
 exports.checkHealth = async (req, res, next) => {
   try {
     await healthService.checkDBConnection();
-    res.status(200).send();
+    res
+      .status(200)
+      .set("Cache-Control", "no-cache, no-store, must-revalidate;")
+      .send();
   } catch (error) {
     next(error);
   }
--- health_check_api/src/middleware/errorHandler.js
+++ health_check_api/src/middleware/errorHandler.js
@@ -6,15 +6,27 @@ const {
 
 function errorHandler(err, req, res, next) {
   if (err instanceof ServiceUnavailableError) {
-    return res.status(503).send();
+    return res
+      .status(503)
+      .set("Cache-Control", "no-cache, no-store, must-revalidate;")
+      .send();
   }
   if (err instanceof MethodNotAllowedError) {
-    return res.status(405).send();
+    return res
+      .status(405)
+      .set("Cache-Control", "no-cache, no-store, must-revalidate;")
+      .send();
   }
   if (err instanceof EndpointnotFound) {
-    return res.status(404).send();
+    return res
+      .status(404)
+      .set("Cache-Control", "no-cache, no-store, must-revalidate;")
+      .send();
   }
-  return res.status(500).send();
+  return res
+    .status(500)
+    .set("Cache-Control", "no-cache, no-store, must-revalidate;")
+    .send();
 }
 
 module.exports = errorHandler;
